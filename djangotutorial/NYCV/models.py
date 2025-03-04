49e994ed343c9f9f9ae95d358ed3b3559faa4770
vpou18 <victormpou@gmail.com>
2025-03-03 22:10:14 -0500

    change

--- djangotutorial/NYCV/models.py
+++
@@ -1,9 +0,0 @@
-class Person(models.Model):
-    class Meta:
-        permissions = [("can_eat_pizzas", "Can eat pizzas")]
-
-
-class Student(Person):
-    class Meta:
-        proxy = True
-        permissions = [("can_deliver_pizzas", "Can deliver pizzas")]
